d43a295219789f9920d3cc2a0b62eb05acc09856
Revi <olliv@outlook.com>
2021-06-14 13:45:02 +0200

    Content Invitation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { Helmet } from 'react-helmet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Main,
+  MainContainer,
+  Left,
+  Right,
+  ContainerSeparator2,
+  DateContent,
+  NamesContent
+} from './styles'
+import { ReactComponent as Separator1 } from './assets/separator1.svg'
+import { ReactComponent as Separator2 } from './assets/separator2.svg'
+import Leones from './assets/leones.png'
+
+const Data = {
+  spanish: {
+    left: {
+      title: 'Querida Familia',
+      paragraph: [
+        'Bienvenidos a nuestra fiesta de boda.',
+        'Queremos compartir con ustedes nuestra alegria y amor.',
+        'Los esperamos.'
+      ],
+      list: {
+        title: 'Lista de regalos:',
+        items: ['Plantas para casa', 'Dinero para Luna de miel']
+      }
+    }
+  },
+  english: {},
+  deutsche: {}
 }
 
-export default App;
+function App() {
+  const [Language, setLanguage] = useState('spanish')
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    setData(Data[Language])
+  }, [Language])
+
+  if (Object.keys(data).length === 0) return <h1>Invitation</h1>
+
+  return (
+    <Main>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Invitation</title>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Parisienne&display=swap'
+          rel='stylesheet'
+        />
+      </Helmet>
+
+      <MainContainer>
+        <Left>
+          <h2>{data.left.title}</h2>
+          {data.left.paragraph.map(text => (
+            <p key={Math.random() * 10}>{text}</p>
+          ))}
+
+          <ul>
+            {data.left.list.items.map(item => (
+              <li key={Math.random() * 10}>{item}</li>
+            ))}
+          </ul>
+        </Left>
+        <ContainerSeparator2>
+          <Separator2 />
+        </ContainerSeparator2>
+        <Right>
+          <h1>Alles ist Liebe</h1>
+          <div>
+            <Separator1 />
+          </div>
+          <NamesContent>REVI & MAY</NamesContent>
+          <DateContent>JULY, 3 2021</DateContent>
+          <img src={Leones} alt='Leones' />
+        </Right>
+      </MainContainer>
+    </Main>
+  )
+}
+
+export default App
---
+++ src/styles.js
@@ -0,0 +1,81 @@
+import styled from 'styled-components/macro'
+
+export const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 35px;
+  box-sizing: border-box;
+`
+
+export const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 10% 1fr;
+  box-sizing: border-box;
+  height: fit-content;
+`
+
+export const ContainerSeparator2 = styled.div`
+  display: flex;
+  align-items: center;
+
+  svg {
+    transform: rotate(90deg) scale(2);
+    width: 116px;
+    height: 100px;
+  }
+`
+
+export const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    margin: 0.67em 0;
+    line-height: 2em;
+  }
+
+  ul {
+    padding-inline-start: 25px;
+    margin: 0;
+  }
+`
+
+export const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-family: 'Parisienne', cursive;
+    font-weight: 500;
+    letter-spacing: 0.01em;
+    margin-bottom: 0;
+  }
+
+  svg {
+    width: 35px;
+  }
+
+  img {
+    margin-top: 10px;
+    width: 100%;
+  }
+`
+
+export const NamesContent = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 300;
+  letter-spacing: 0.35em;
+  font-size: 0.9em;
+  margin: 5px;
+`
+
+export const DateContent = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 400;
+  letter-spacing: 0.35em;
+  font-size: 0.75em;
+  color: #e0b650;
+`
